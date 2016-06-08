b242e635355ddf9f2273d5bfc9545d4eb1e625bc
Brad London <london.brad@gmail.com>
2016-06-08 11:24:06 -0700

    add constans file

---
+++ app/src/main/java/com/eberhealth/eberhealthprovider/Constants.java
@@ -0,0 +1,9 @@
+package com.eberhealth.eberhealthprovider;
+
+/**
+ * Created by owner on 6/8/16.
+ */
+public class Constants {
+    public static final String PREFERENCES_UID_KEY = "uid";
+    public static final String FIREBASE_CHILD_USERS = "users";
+}
