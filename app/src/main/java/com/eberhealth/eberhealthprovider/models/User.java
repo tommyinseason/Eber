7b1ad576958f0b2fb24ed415e379d460354bcb2b
Brad London <london.brad@gmail.com>
2016-06-08 11:21:42 -0700

    add user model

---
+++ app/src/main/java/com/eberhealth/eberhealthprovider/models/User.java
@@ -0,0 +1,26 @@
+package com.eberhealth.eberhealthprovider.models;
+
+/**
+ * Created by owner on 6/8/16.
+ */
+public class User {
+    private String name;
+    private String email;
+
+    public User() {}
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
+
+
